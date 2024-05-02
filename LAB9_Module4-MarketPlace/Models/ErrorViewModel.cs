49d49d63ed243282802205e0d2a59255923758df
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-05-03 00:24:31 +0300

    Add project files.

---
+++ LAB9_Module4-MarketPlace/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LAB9_Module4_MarketPlace.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
