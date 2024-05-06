7d4deb179053a945a2ea4bdb9fb0485f4f26c685
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-05-06 17:57:21 +0300

    fixed small css elements

--- LAB9_Module4-MarketPlace/Controllers/HomeController.cs
+++ LAB9_Module4-MarketPlace/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace LAB9_Module4_MarketPlace.Controllers
         {
             return View();
         }
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
