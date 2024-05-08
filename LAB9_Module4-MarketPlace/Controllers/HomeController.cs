c0645650e022201af35db62aaafe4caa27f9ede6
kriomanser2000 <160469777+kriomanser2000@users.noreply.github.com>
2024-05-08 17:10:33 +0300

    added page Cart

--- LAB9_Module4-MarketPlace/Controllers/HomeController.cs
+++ LAB9_Module4-MarketPlace/Controllers/HomeController.cs
@@ -34,6 +34,10 @@ namespace LAB9_Module4_MarketPlace.Controllers
         {
             return View();
         }
+        public IActionResult Cart()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
